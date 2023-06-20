4dbe8dd8df75355d9e297beacf949cf923b295f7
Njalale <fnjalale@gmail.com>
2023-06-20 20:44:47 +0300

    Completed bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,9 @@ class StudentsController < ApplicationController
         grades = Student.all.order(grade: :desc )
         render json: grades
     end
+    
+    def highest_grade
+        highest_grade = Student.all.order(grade: :desc).first
+        render json: highest_grade
+    end
 end
